7f8c422b2547f8464f63ca1b95dd0d22a2aff1fa
8345738475 <32421222@googlepriv.com>
2020-10-10 07:20:38 +0300

    replace old xorstr

--- client/client/src/auth/validation.cpp
+++ client/client/src/auth/validation.cpp
@@ -2,6 +2,7 @@
 #include "../crypto/xorstr.hpp"
 #include "../http/http.hpp"
 using namespace ls;
+using namespace hide_string;
 
 validation::validation(
     const std::string_view& server_url,
@@ -12,7 +13,7 @@ validation::validation(
 }
 
 #define json_ptr(data)\
-    nlohmann::json::json_pointer(xors(data))
+    nlohmann::json::json_pointer((LPCSTR)xors(data))
 
 template<typename type>
 constexpr __forceinline
@@ -29,8 +30,8 @@ bool validation::login(
     }
 
     const auto j = make_request( request_type::login, {
-        { xors( "username" ), username },
-        { xors( "hwid" ), hwid::get_super_secret_info() }
+        { (LPCSTR)xors( "username" ), username },
+        { (LPCSTR)xors( "hwid" ), hwid::get_super_secret_info() }
     } );
 
     const std::int32_t code = j.at( json_ptr( "/code" ) );
@@ -48,7 +49,7 @@ bool validation::get_subscriptions(
     std::vector<subscription_type>& subs ) const
 {
     const auto j = make_request( request_type::get_subs, {
-        { xors( "user_id" ), _user_id }
+        { (LPCSTR)xors( "user_id" ), _user_id }
     } );
 
     const std::int32_t code = j.at( json_ptr( "/code" ) );
@@ -72,15 +73,14 @@ bool validation::get_product(
     std::string&      data ) const
 {
     const auto j = make_request( request_type::get_product, {
-        { xors( "product_id" ), id }
+        { (LPCSTR)xors( "product_id" ), id }
     } );
-
+    
     const std::int32_t code = j.at( json_ptr( "/code" ) );
     if( static_cast<api_code>( code ) != api_code::success ) {
         return false;
     }
-
-    data = j.at( json_ptr( xors( "/response" ) ) );
+    data = j.at( json_ptr("/response") );
 
     return true;
 }
@@ -91,39 +91,39 @@ nlohmann::json validation::make_request(
 {
     const sealedbox::key_pair kp;
     nlohmann::json request_data;
-    request_data[ xors( "request" ) ]    = static_cast<std::underlying_type_t<request_type>>( type );
-    request_data[ xors( "public_key" ) ] = kp.get_public_key();
+    request_data[(LPCSTR)xors( "request" ) ]    = static_cast<std::underlying_type_t<request_type>>( type );
+    request_data[(LPCSTR)xors( "public_key" ) ] = kp.get_public_key();
 
     if( !data.empty() ) {
-        request_data[ xors( "data" ) ] = data;
+        request_data[(LPCSTR)xors( "data" ) ] = data;
     }
 
     const auto http_response = http_post(
         _server_url,
-        xors( "client" ),
-        std::string( xors( "data=" ) ) + base64::url_encode( sealedbox::encrypt( request_data.dump(), _server_key ) )
+        (LPCSTR)xors( "client" ),
+        std::string((LPCSTR)xors( "data=" ) ) + base64::url_encode( sealedbox::encrypt( request_data.dump(), _server_key ) )
     );
 
     if( http_response.empty() ) {
-        return { { xors( "code" ), enum_v( api_code::client_empty_resonse ) } };
+        return { { (LPCSTR)xors( "code" ), enum_v( api_code::client_empty_resonse ) } };
     }
-    if( http_response == xors( "Bad Request" ) || http_response == xors( "Unauthorized" ) || http_response == xors( "Conflict" ) ) {
-        return { { xors( "code" ), enum_v( api_code::client_bad_response ) } };
+    if( http_response == (LPCSTR)xors( "Bad Request" ) || http_response == (LPCSTR)xors( "Unauthorized" ) || http_response == (LPCSTR)xors( "Conflict" ) ) {
+        return { { (LPCSTR)xors( "code" ), enum_v( api_code::client_bad_response ) } };
     }
     try {
         const auto decrypted_response = sealedbox::decrypt( http_response, kp );
         if( decrypted_response.empty() ) {
-            return { { xors( "code" ), enum_v( api_code::client_weird_response ) } };
+            return { { (LPCSTR)xors( "code" ), enum_v( api_code::client_weird_response ) } };
         }
 
         std::istringstream iss( decrypted_response );
         if( !iss ) {
-            return { { xors( "code" ), enum_v( api_code::client_weird_response ) } };
+            return { { (LPCSTR)xors( "code" ), enum_v( api_code::client_weird_response ) } };
         }
 
         return nlohmann::json::parse( iss );
     }
     catch( ... ) {
-        return { { xors( "code" ), enum_v( api_code::client_weird_response ) } };
+        return { { (LPCSTR)xors( "code" ), enum_v( api_code::client_weird_response ) } };
     }
 }
