869e6e1c2c30ee414b971a93509c165947cd10fb
houp <xixjx@email.no>
2021-04-24 03:21:28 +0300

    nvm

--- client/client/src/main.cpp
+++ client/client/src/main.cpp
@@ -11,9 +11,7 @@ std::int32_t main()
     sodium_init();
 
     ls::validation v( "localhost:8001", "DUx1Laid4QYY2IhUl0jg9JyP74y7esKDCSVn49Ix6Fc=" );
-     // 400: Bad Request
-     // 401: Unauthorized
-    if( !v.login( "testd" ) ) {
+    if( !v.login(   "testd" ) ) {
         return -1;
     }
 
@@ -22,10 +20,10 @@ std::int32_t main()
         return -1;
     }
 
-    std::string file;
+  /*  std::string file;
     if( !v.get_product( std::get<1>( subs.front() ), file ) ) {
         return -1;
-    }
+    }*/
 
     printf( "ok!\n" );
     getchar();
